fb307d0cf4726b34771881420ecb1a3678afa3a8
phoebeykq <113828692+phoebeykq@users.noreply.github.com>
2023-03-18 13:33:21 +0800

    added response message to UserController

--- m3project/src/main/java/sg/edu/ntu/m3project/m3project/controller/UserController.java
+++ m3project/src/main/java/sg/edu/ntu/m3project/m3project/controller/UserController.java
@@ -6,12 +6,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 
 import java.util.List;
 import java.util.Optional;
 
 import sg.edu.ntu.m3project.m3project.repository.UserRepository;
 import sg.edu.ntu.m3project.m3project.entity.UserEntity;
+import main.java.sg.edu.ntu.m3project.m3project.helper.ResponseMessage;
 
 @RestController
 @RequestMapping("/users")
@@ -27,12 +29,13 @@ public class UserController {
     }
 
     @RequestMapping(value="/{id}", method = RequestMethod.GET)
-    public ResponseEntity<UserEntity> findById(@PathVariable int id) {
+    public ResponseEntity findById(@PathVariable int id) {
         Optional<UserEntity> optional = userRepo.findById(id);
         if (optional.isPresent()) {
             UserEntity user = optional.get();
             return ResponseEntity.ok().body(user);
         }
-        return ResponseEntity.notFound().build();
+        //return ResponseEntity.notFound().build();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ResponseMessage("Error 404 - User ID Not Found"));
     }
 }
