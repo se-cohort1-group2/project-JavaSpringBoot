a7f6d741be9e9b52445cf3649d7b88d77acf9dc9
xong002 <xong002@gmail.com>
2023-03-20 16:59:02 +0800

    cleanup unused imports

--- m3project/src/main/java/sg/edu/ntu/m3project/m3project/controller/TicketController.java
+++ m3project/src/main/java/sg/edu/ntu/m3project/m3project/controller/TicketController.java
@@ -1,8 +1,5 @@
 package sg.edu.ntu.m3project.m3project.controller;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,11 +10,8 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import sg.edu.ntu.m3project.m3project.entity.TicketEntity;
-import sg.edu.ntu.m3project.m3project.entity.UserEntity;
 import sg.edu.ntu.m3project.m3project.helper.NewTicket;
 import sg.edu.ntu.m3project.m3project.helper.ResponseMessage;
 import sg.edu.ntu.m3project.m3project.repository.TicketRepository;
