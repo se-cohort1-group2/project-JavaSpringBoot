6ce6260af5c798dcf5a50201af43859755b50e78
phoebeykq <113828692+phoebeykq@users.noreply.github.com>
2023-03-27 06:42:32 +0800

    refactored out DisplayUser from PageUserByID

---
+++ m3project-ReactJS/src/components/DisplayUser.js
@@ -0,0 +1,165 @@
+import style from "../routes/Page.module.css"; 
+
+import { useContext, useState } from "react"; 
+
+import LoginContext from "../context/LoginContext"; 
+import localAPI from "../api/localAPI"; 
+
+import VisibilityOn from "../images/VisibilityOn.svg"; 
+import VisibilityOff from "../images/VisibilityOff.svg"; 
+
+function formatPassword(x) {
+    let maskedPassword = ""; 
+    for (let i = 0; i < x.length; i++) {
+        maskedPassword += "*"; 
+    }
+    return maskedPassword; 
+}
+
+const blankUpdateForm = {
+    "name": "",
+    "phone": "",
+    "email": "",
+    "password": ""
+}
+
+function DisplayUser({ UsersList, getUsers, displayID }) {
+
+    const LoginCtx = useContext(LoginContext); 
+
+    const [isEditing, setIsEditing] = useState(false); 
+    const [UpdateForm, setUpdateForm] = useState(blankUpdateForm); 
+    const [ShowPassword, setShowPassword] = useState(false); 
+
+    const toggleVisibility = () => {
+        ShowPassword ? setShowPassword(false) : setShowPassword(true)
+    }
+
+    // eslint-disable-next-line
+    const user = UsersList.find(({ id }) => id == displayID); 
+
+    const handleEdit = () => {
+        setUpdateForm({
+            "name": user.name,
+            "phone": user.phone,
+            "email": user.email,
+            "password": user.password
+        })
+        setIsEditing(true); 
+    }
+
+    const handleInput = (e) => {
+        setUpdateForm((prevState) => {
+            return {
+                ...prevState, 
+                [e.target.name]: e.target.value, 
+            }
+        })
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault(); 
+        await updateUser(UpdateForm); 
+        setIsEditing(false); 
+        getUsers(); 
+    }
+
+    const updateUser = async (updatedData) => {
+        try {
+            const config = {
+                headers: {"user-id": LoginCtx.userID}
+            }
+            const response = await localAPI.put(`/users/${user.id}`, updatedData, config)
+            console.log("Successfully updated!", response.data)
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
+
+    return (
+        <>
+        <div style={{textAlign: "center"}}>
+            {user && 
+            <>
+            {!isEditing && (
+            <table className={style.MiniTable}>
+                <tbody>
+                    <tr>
+                        <th>User ID:</th>
+                        <td>{user.id}</td>
+                    </tr>
+                    <tr>
+                        <th>Name:</th>
+                        <td>{String(user.name)}</td>
+                    </tr>
+                    <tr>
+                        <th>Phone:</th>
+                        <td>{String(user.phone)}</td>
+                    </tr>
+                    <tr>
+                        <th>Email:</th>
+                        <td>{user.email}</td>
+                    </tr>
+                    <tr>
+                        <th>Password:</th>
+                        <td>{formatPassword(user.password)}</td>
+                    </tr>
+                </tbody>
+            </table>
+            )}
+            {isEditing && (<form onSubmit={handleSubmit}>
+            <table className={style.MiniTable}>
+                <tbody>
+                    <tr>
+                        <th>User ID:</th>
+                        <td>{user.id}</td>
+                    </tr>
+                    <tr>
+                        <th>Name:</th>
+                        <td>
+                            <input value={UpdateForm.name} name="name" type="text" onChange={(e) => handleInput(e, "name")}/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>Phone:</th>
+                        <td>
+                            <input value={UpdateForm.phone} name="phone" type="tel" onChange={(e) => handleInput(e, "phone")}/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>Email:</th>
+                        <td>
+                            <input value={UpdateForm.email} name="email" type="email" required onChange={(e) => handleInput(e, "email")}/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>Password:</th>
+                        <td>
+                            <input value={UpdateForm.password} name="password" type={ShowPassword ? "text" : "password"} required onChange={(e) => handleInput(e, "password")}/>
+                            {ShowPassword ?
+                            <img onClick={toggleVisibility} className={style.VisibilityOnOff} alt="VisibilityOn" src={VisibilityOn}/>
+                            :
+                            <img onClick={toggleVisibility} className={style.VisibilityOnOff} alt="VisibilityOff" src={VisibilityOff}/>
+                            }
+                        </td>
+                    </tr>
+                    <tr>
+                        <th></th>
+                        <td>
+                            <input value="Save" type="submit"/>
+                            &nbsp;&nbsp;
+                            <button onClick={() => setIsEditing(false)}>Cancel</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            </form>)}
+            {isEditing ? <></> : <><button onClick={() => handleEdit()}>Edit user profile</button><br/><br/></>}
+            </>
+            }
+        </div>
+        </>
+    )
+}
+
+export default DisplayUser; 
--- m3project-ReactJS/src/routes/PageUserByID.js
+++ m3project-ReactJS/src/routes/PageUserByID.js
@@ -1,150 +1,18 @@
 import style from "./Page.module.css"; 
 
-import { useContext, useState } from "react"; 
 import { useParams, Link } from "react-router-dom"; 
 
-import LoginContext from "../context/LoginContext"; 
-import localAPI from "../api/localAPI"; 
-
-const blankUpdateForm = {
-    "name": "",
-    "phone": "",
-    "email": "",
-    "password": ""
-}
+import DisplayUser from "../components/DisplayUser"; 
 
 function PageUserByID({ UsersList, getUsers }) {
 
-    const LoginCtx = useContext(LoginContext); 
-
-    const [isEditing, setIsEditing] = useState(false); 
-    const [UpdateForm, setUpdateForm] = useState(blankUpdateForm); 
-
     const {UserID} = useParams(); 
-
-    // eslint-disable-next-line
-    const user = UsersList.find(({ id }) => id == UserID); 
-
-    /* console.log("-----", user)
-    if (typeof user === "object") {
-        console.log("-----", user.id)
-    } */
-
-    const handleEdit = () => {
-        setUpdateForm({
-            "name": user.name,
-            "phone": user.phone,
-            "email": user.email,
-            "password": user.password
-        })
-        setIsEditing(true); 
-    }
-
-    const handleInput = (e) => {
-        setUpdateForm((prevState) => {
-            return {
-                ...prevState, 
-                [e.target.name]: e.target.value, 
-            }
-        })
-    }
-
-    const handleSubmit = async (e) => {
-        e.preventDefault(); 
-        await updateUser(UpdateForm); 
-        setIsEditing(false); 
-        //window.location.reload(); 
-        getUsers(); 
-    }
-
-    const updateUser = async (updatedData) => {
-        try {
-            const config = {
-                headers: {"user-id": LoginCtx.userID} // user.id // LoginCtx.userID
-            }
-            const response = await localAPI.put(`/users/${user.id}`, updatedData, config)
-            console.log("Successfully updated!", response.data)
-        } catch (error) {
-            console.log(error.message)
-        }
-    }
 
     return (
         <>
         <div className={style.page}>
             <div className={style.title}>View User by ID</div>
-            {user && 
-            <>
-            <button onClick={() => handleEdit()}>Update user profile</button>
-            <br/><br/>
-            {!isEditing && (
-            <table className={style.MiniTable}>
-                <tbody>
-                    <tr>
-                        <th>User ID:</th>
-                        <td>{user.id}</td>
-                    </tr>
-                    <tr>
-                        <th>Name:</th>
-                        <td>{String(user.name)}</td>
-                    </tr>
-                    <tr>
-                        <th>Phone:</th>
-                        <td>{String(user.phone)}</td>
-                    </tr>
-                    <tr>
-                        <th>Email:</th>
-                        <td>{user.email}</td>
-                    </tr>
-                    <tr>
-                        <th>Password:</th>
-                        <td>{user.password}</td>
-                    </tr>
-                </tbody>
-            </table>
-            )}
-            {isEditing && (<form onSubmit={handleSubmit}>
-            <table className={style.MiniTable}>
-                <tbody>
-                    <tr>
-                        <th>User ID:</th>
-                        <td>{user.id}</td>
-                    </tr>
-                    <tr>
-                        <th>Name:</th>
-                        <td>
-                            <input value={UpdateForm.name} name="name" type="text" onChange={(e) => handleInput(e, "name")}/>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>Phone:</th>
-                        <td>
-                            <input value={UpdateForm.phone} name="phone" type="tel" onChange={(e) => handleInput(e, "phone")}/>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>Email:</th>
-                        <td>
-                            <input value={UpdateForm.email} name="email" type="email" required onChange={(e) => handleInput(e, "email")}/>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>Password:</th>
-                        <td>
-                            <input value={UpdateForm.password} name="password" type="password" required onChange={(e) => handleInput(e, "password")}/>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th></th>
-                        <td>
-                            <input value="Save" type="submit"/>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-            </form>)}
-            </>
-            }
+            <DisplayUser UsersList={UsersList} getUsers={getUsers} displayID={UserID}/>
             <Link to="/users" className={style.BackLink}>
                 <span className={style.BackArrow}>🡄</span>
                 <span className={style.BackText}>&nbsp;Back</span>
