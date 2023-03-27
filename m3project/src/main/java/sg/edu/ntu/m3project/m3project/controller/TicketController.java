6695d283c3588c27509f72f63eeebea68371afa7
xong002 <xong002@gmail.com>
2023-03-27 18:07:15 +0800

    added GlobalExceptionHandler and UserNotFoundException

--- m3project/src/main/java/sg/edu/ntu/m3project/m3project/controller/TicketController.java
+++ m3project/src/main/java/sg/edu/ntu/m3project/m3project/controller/TicketController.java
@@ -3,7 +3,6 @@ package sg.edu.ntu.m3project.m3project.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import sg.edu.ntu.m3project.m3project.helper.NewTicket;
-import sg.edu.ntu.m3project.m3project.helper.ResponseMessage;
 import sg.edu.ntu.m3project.m3project.service.TicketService;
 
 @CrossOrigin
@@ -27,20 +25,11 @@ public class TicketController {
     @Autowired
     TicketService ticketService;
 
-    // Get Tickets by All/User ID (to refactor to ticketService)
+    
+    // Get Tickets by All/User ID
     @GetMapping
     public ResponseEntity<?> findAllById(@RequestHeader(value = "user_id", required = false) Integer userId) {
-        try {
-            return ticketService.find(userId);
-        } catch (IllegalArgumentException iae) {
-            iae.printStackTrace();
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body(new ResponseMessage("Bad request."));
-        } catch (Exception e) {
-            e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(new ResponseMessage("Something went wrong. Please try again later."));
-        }
+        return ticketService.find(userId);
     }
 
     // Create Ticket (to update to array input for multiple tickets)
@@ -48,13 +37,7 @@ public class TicketController {
     public ResponseEntity<?> createTicket(
             @RequestHeader(value = "user_id") int userId,
             @RequestBody List<NewTicket> newTickets) {
-        try {
             return ticketService.add(userId, newTickets);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(new ResponseMessage("Something went wrong. Please try again later."));
-        }
     }
 
     @PutMapping("/{ticket_id}") // change selectedSeatId to RequestParam?
@@ -62,26 +45,14 @@ public class TicketController {
             @RequestHeader(value = "user_id") int userId,
             @PathVariable int ticket_id,
             @RequestBody String selectedSeatId) {
-        try {
             return ticketService.changeSeat(userId, ticket_id, selectedSeatId);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(new ResponseMessage("Something went wrong. Please try again later."));
-        }
     }
 
     @PostMapping("/{ticket_id}/delete")
     public ResponseEntity<?> deleteTicket(
             @RequestHeader(value = "user_id") int userId,
             @PathVariable int ticket_id) {
-        try {
             return ticketService.delete(userId, ticket_id);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(new ResponseMessage("Something went wrong. Please try again later."));
-        }
     }
 
 }
---
+++ m3project/src/main/java/sg/edu/ntu/m3project/m3project/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package sg.edu.ntu.m3project.m3project.exceptions;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import sg.edu.ntu.m3project.m3project.helper.ResponseMessage;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<?> handleUserNotFoundException(Exception ex){
+        ex.printStackTrace();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+            .body(new ResponseMessage("User not found."));
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<?> handleRuntimeException(Exception ex){
+        ex.printStackTrace();
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .body(new ResponseMessage("Something went wrong. Please try again later."));
+    }
+}
