f39fc4dce341673c1c01c246d8dd3b80949cb23a
phoebeykq <113828692+phoebeykq@users.noreply.github.com>
2023-03-27 23:10:35 +0800

    refactored and consolidated UserController, UserService, UserRepository

--- m3project/src/main/java/sg/edu/ntu/m3project/m3project/controller/UserController.java
+++ m3project/src/main/java/sg/edu/ntu/m3project/m3project/controller/UserController.java
@@ -9,21 +9,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.HttpStatus;
-
-import java.util.List;
-import java.util.Optional;
-import java.util.Date;
-import java.sql.Timestamp;
 
 import sg.edu.ntu.m3project.m3project.repository.UserRepository;
 import sg.edu.ntu.m3project.m3project.service.UserService;
 import sg.edu.ntu.m3project.m3project.entity.UserEntity;
-import sg.edu.ntu.m3project.m3project.helper.ResponseMessage;
 
 @CrossOrigin
 @RestController
-@RequestMapping("/users")
+@RequestMapping
 public class UserController {
 
     @Autowired
@@ -32,89 +25,28 @@ public class UserController {
     @Autowired
     UserService userService;
 
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(value = "/users", method = RequestMethod.GET)
     public ResponseEntity<?> findAll() {
-        try {
-            List<UserEntity> users = (List<UserEntity>) userRepo.findAll();
-            return ResponseEntity.ok().body(users);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(new ResponseMessage("Something went wrong. Please try again later."));
-        }
+        return userService.findAll();
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    @RequestMapping(value = "/users/{id}", method = RequestMethod.GET)
     public ResponseEntity<?> findById(@PathVariable int id) {
-        try {
-            Optional<UserEntity> optional = userRepo.findById(id);
-            if (optional.isPresent()) {
-                UserEntity user = optional.get();
-                return ResponseEntity.ok().body(user);
-            }
-            return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                    .body(new ResponseMessage("User " + id + " not found. Please try a different User ID."));
-        } catch (Exception e) {
-            e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(new ResponseMessage("Something went wrong. Please try again later."));
-        }
+        return userService.findById(id);
     }
 
-    @RequestMapping(method = RequestMethod.POST)
+    @RequestMapping(value = "/users/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<?> update(@PathVariable int id, @RequestBody UserEntity user, @RequestHeader(name = "user-id", required = true) int userID) {
+        return userService.update(id, user, userID);
+    }
+
+    @RequestMapping(value = "/users/register", method = RequestMethod.POST)
     public ResponseEntity<?> create(@RequestBody UserEntity user) {
-        try {
-            if (userRepo.existsByEmail(user.getEmail())) {
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                        .body(new ResponseMessage("Sorry, a user with this email already exists."));
-            }
-            UserEntity createNewUser = userRepo.save(user);
-            return new ResponseEntity<>(userRepo.findById(createNewUser.getId()), HttpStatus.CREATED);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(new ResponseMessage("Something went wrong. Please try again later."));
-        }
+        return userService.create(user);
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-    public ResponseEntity<?> update(@RequestHeader(name = "user-id", required = true) int userID,
-            @RequestBody UserEntity user, @PathVariable int id) {
-        try {
-            Optional<UserEntity> userToBeUpdated = userRepo.findById(id);
-            if (userToBeUpdated.isPresent()) {
-                UserEntity editedUser = userToBeUpdated.get();
-
-                // check if userInHeader exists
-                Optional<UserEntity> userInHeader = userRepo.findById(userID);
-                if (userInHeader.isPresent()) {
-                    UserEntity headerUser = userInHeader.get();
-
-                    // check if match - editedUser.getId() == userID
-                    // check if admin - headerUser.isAdminStatus()
-                    if (userID == editedUser.getId() || headerUser.isAdminStatus()) {
-                        Timestamp updatedAt = new Timestamp(new Date().getTime());
-                        editedUser.setName(user.getName());
-                        editedUser.setPhone(user.getPhone());
-                        editedUser.setEmail(user.getEmail());
-                        editedUser.setPassword(user.getPassword());
-                        /* editedUser.setAdminStatus(user.isAdminStatus()); */
-                        editedUser.setUpdatedAt(updatedAt);
-                        editedUser = userRepo.save(editedUser);
-                        return ResponseEntity.ok().body(editedUser);
-                    }
-                    return ResponseEntity.status(HttpStatus.FORBIDDEN)
-                            .body(new ResponseMessage("Sorry, you are not allowed to update this user."));
-                }
-                return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                        .body(new ResponseMessage("Invalid credentials."));
-            }
-            return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                    .body(new ResponseMessage("User " + id + " not found. Please try a different User ID."));
-        } catch (Exception e) {
-            e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(new ResponseMessage("Something went wrong. Please try again later."));
-        }
+    @RequestMapping(value = "/users/login", method = RequestMethod.POST)
+    public ResponseEntity<?> login(@RequestBody UserEntity user) {
+        return userService.login(user);
     }
 }
--- m3project/src/main/java/sg/edu/ntu/m3project/m3project/service/UserService.java
+++ m3project/src/main/java/sg/edu/ntu/m3project/m3project/service/UserService.java
@@ -1,25 +1,26 @@
 package sg.edu.ntu.m3project.m3project.service;
 
-import java.nio.file.AccessDeniedException;
-import java.security.SecureRandom;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
-
+import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Service;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 
+import java.nio.file.AccessDeniedException;
+import java.security.SecureRandom;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
+import java.util.Date;
+import java.sql.Timestamp;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
+import sg.edu.ntu.m3project.m3project.repository.UserRepository;
 import sg.edu.ntu.m3project.m3project.entity.UserEntity;
 import sg.edu.ntu.m3project.m3project.helper.ResponseMessage;
-import sg.edu.ntu.m3project.m3project.repository.UserRepository;
 
 @Service
 public class UserService {
@@ -33,59 +34,140 @@ public class UserService {
     SecureRandom bCryptSR = new SecureRandom();
     BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder(10, bCryptSR);
 
+    //yongxin
     public Map<String, String> generateToken(UserEntity user) {
-
         String jwtToken = Jwts.builder()
                 .setSubject(user.getEmail())
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + 864000000)) // 10 days
                 .signWith(SignatureAlgorithm.HS256, secret)
                 .compact();
-
         Map<String, String> jwtTokenGen = new HashMap<>();
         jwtTokenGen.put("token", jwtToken);
         return jwtTokenGen;
     }
 
-    public UserEntity getUserForAuth(String email, String password) throws AccessDeniedException {
-        Optional<UserEntity> optionalUser = userRepo.findByEmail(email);
-
-        if (!optionalUser.isPresent()) {
-            throw new AccessDeniedException("Invalid email.");
-        }
-
-        UserEntity foundUser = optionalUser.get();
-
-        if (!bCryptPasswordEncoder.matches(password, foundUser.getPassword())) {
-            throw new AccessDeniedException("Invalid password.");
-
-        }
-
-        return foundUser;
-
-    }
-
+    //yongxin
     public String checkToken(String token) throws AccessDeniedException {
-
         try {
             String email = Jwts.parser()
                     .setSigningKey(secret)
                     .parseClaimsJws(token)
                     .getBody()
                     .getSubject();
-
             return email;
         } catch (Exception e) {
             throw new AccessDeniedException("Invalid token");
         }
     }
 
+    //yongxin
     public UserEntity hashPassword(UserEntity user) {
         String hashPassword = bCryptPasswordEncoder.encode(user.getPassword());
         user.setPassword(hashPassword);
         return user;
     }
 
+    //phoebe
+    public ResponseEntity<?> findAll() {
+        try {
+            List<UserEntity> users = (List<UserEntity>) userRepo.findAll();
+            return ResponseEntity.ok().body(users);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ResponseMessage("Something went wrong. Please try again later."));
+        }
+    }
+
+    //phoebe
+    public ResponseEntity<?> findById(int id) {
+        try {
+            Optional<UserEntity> userByID = userRepo.findById(id);
+            if (userByID.isPresent()) {
+                UserEntity user = userByID.get();
+                return ResponseEntity.ok().body(user);
+            }
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(new ResponseMessage("User " + id + " not found. Please try a different User ID."));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ResponseMessage("Something went wrong. Please try again later."));
+        }
+    }
+
+    //phoebe
+    public ResponseEntity<?> update(int id, UserEntity user, int userID) {
+        try {
+            Optional<UserEntity> userToBeUpdated = userRepo.findById(id);
+            if (userToBeUpdated.isPresent()) {
+                UserEntity editedUser = userToBeUpdated.get();
+
+                // check if userInHeader exists
+                Optional<UserEntity> userInHeader = userRepo.findById(userID);
+                if (userInHeader.isPresent()) {
+                    UserEntity headerUser = userInHeader.get();
+
+                    // check if match - editedUser.getId() == userID
+                    // check if admin - headerUser.isAdminStatus()
+                    if (userID == editedUser.getId() || headerUser.isAdminStatus()) {
+                        Timestamp updatedAt = new Timestamp(new Date().getTime());
+                        editedUser.setName(user.getName());
+                        editedUser.setPhone(user.getPhone());
+                        editedUser.setEmail(user.getEmail());
+                        editedUser.setPassword(user.getPassword() != null ? hashPassword(user).getPassword() : null);
+                        /* editedUser.setAdminStatus(user.isAdminStatus()); */
+                        editedUser.setUpdatedAt(updatedAt);
+                        editedUser = userRepo.save(editedUser);
+                        return ResponseEntity.ok().body(editedUser);
+                    }
+                    return ResponseEntity.status(HttpStatus.FORBIDDEN)
+                            .body(new ResponseMessage("Sorry, you are not allowed to update this user."));
+                }
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                        .body(new ResponseMessage("Invalid credentials."));
+            }
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(new ResponseMessage("User " + id + " not found. Please try a different User ID."));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ResponseMessage("Something went wrong. Please try again later."));
+        }
+    }
+
+    //phoebe
+    public ResponseEntity<?> create(UserEntity user) {
+        try {
+            if (userRepo.existsByEmail(user.getEmail())) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                        .body(new ResponseMessage("Sorry, a user with this email already exists."));
+            }
+            UserEntity hashedUser = hashPassword(user);
+            UserEntity createNewUser = userRepo.save(user);
+            return new ResponseEntity<>(userRepo.findById(createNewUser.getId()), HttpStatus.CREATED);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ResponseMessage("Something went wrong. Please try again later."));
+        }
+    }
+
+    //yongxin
+    public UserEntity getUserForAuth(String email, String password) throws AccessDeniedException {
+        Optional<UserEntity> optionalUser = userRepo.findByEmail(email);
+        if (!optionalUser.isPresent()) {
+            throw new AccessDeniedException("User not found, please try a different email.");
+        }
+        UserEntity foundUser = optionalUser.get();
+        if (!bCryptPasswordEncoder.matches(password, foundUser.getPassword())) {
+            throw new AccessDeniedException("Wrong password for '" + email + "'. Please try again.");
+        }
+        return foundUser;
+    }
+
+    //yongxin
     public ResponseEntity<?> login(UserEntity user) {
         try {
             if (user.getEmail() == null || user.getPassword() == null) {
@@ -103,10 +185,4 @@ public class UserService {
                     .body(new ResponseMessage("Something went wrong. Please try again later."));
         }
     }
-
-    public ResponseEntity<?> register(UserEntity user) {
-        UserEntity newUser = userRepo.save(hashPassword(user));
-        return new ResponseEntity<>(userRepo.findById(newUser.getId()), HttpStatus.CREATED);
-    }
-
 }
