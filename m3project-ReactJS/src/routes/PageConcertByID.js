ed2846238319577dfb351366703d8cd3a71a3edf
phoebeykq <113828692+phoebeykq@users.noreply.github.com>
2023-03-29 10:52:33 +0800

    added Purchase functionality

--- m3project-ReactJS/src/routes/PageConcertByID.js
+++ m3project-ReactJS/src/routes/PageConcertByID.js
@@ -2,65 +2,19 @@ import style from "./Page.module.css";
 
 import { useParams, Link } from "react-router-dom"; 
 
-function formatDate(x) {
-    return new Date(x).toLocaleString("en-SG", {
-        weekday: "short", 
-        day: "numeric", 
-        month: "short", 
-        year: "numeric", 
-        hour: "numeric", 
-        minute: "2-digit", 
-    })
-}
+import DisplayConcert from "../components/DisplayConcert"; 
 
-function PageConcertByID({ ConcertsList }) {
+function PageConcertByID({ ConcertsList, TicketsList, SeatsList, getTickets }) {
 
     const {ConcertID} = useParams(); 
 
-    // eslint-disable-next-line
-    const concert = ConcertsList.find(({ id }) => id == ConcertID); 
-
     return (
         <>
-            <div className={style.page}>
-                <div className={style.title}>View Concert by ID</div>
-                {concert && 
-                    <>
-                    <table className={style.MiniTable}>
-                        <tbody>
-                            <tr>
-                                <th>Concert ID:</th>
-                                <td>{concert.id}</td>
-                            </tr>
-                            <tr>
-                                <th>Artist:</th>
-                                <td>{concert.artist}</td>
-                            </tr>
-                            <tr>
-                                <th>Concert Date & Time:</th>
-                                <td>{formatDate(concert.concertDate)}</td>
-                            </tr>
-                            <tr>
-                                <th>Concert Venue:</th>
-                                <td>{concert.concertVenue}</td>
-                            </tr>
-                            <tr>
-                                <th>Tickets Available:</th>
-                                <td>{concert.ticketsAvailable}</td>
-                            </tr>
-                            <tr>
-                                <th>Ticket Price:</th>
-                                <td>{concert.ticketPrice}</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                    </>
-                }
-                <Link to="/concerts" className={style.BackLink}>
-                    <span className={style.BackArrow}>🡄</span>
-                    <span className={style.BackText}>&nbsp;Back</span>
-                </Link>
-            </div>
+            <DisplayConcert ConcertID={ConcertID} ConcertsList={ConcertsList} TicketsList={TicketsList} SeatsList={SeatsList} getTickets={getTickets}/>
+            <Link to="/concerts" className={style.BackLink}>
+                <span className={style.BackArrow}>🡄</span>
+                <span className={style.BackText}>&nbsp;Back</span>
+            </Link>
         </>
     )
 }
