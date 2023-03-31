037074de0b90e940a96e97aee7ea23d84d31bc85
phoebeykq <113828692+phoebeykq@users.noreply.github.com>
2023-03-31 14:05:44 +0800

    updated HomePage to match main branch

--- m3project-ReactJS/src/routes/DefaultHomePage.js
+++ m3project-ReactJS/src/routes/DefaultHomePage.js
@@ -1,14 +1,46 @@
 import style from "./Page.module.css"; 
+import style2 from "../components/Table.module.css"; 
+
+import { useContext } from "react"; 
+import { Link } from "react-router-dom"; 
+
+import LoginContext from "../context/LoginContext"; 
 
 function DefaultHomePage() {
+
+    const LoginCtx = useContext(LoginContext); 
+
+    if (!LoginCtx.isLoggedIn) {
     return (
         <>
             <div className={style.page}>
-                <div className={style.title}>Home</div>
-                Click on the tabs to view data
+                <div style={{ fontSize: "1.75rem", margin: "2.2em 0 0 0", fontWeight: "bold" }}>Home</div>
+                <br/><br/>
+                <Link to="/login">
+                    <button className={style2.PurchaseBtn}>Login</button>
+                </Link>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <Link to="/login/register">
+                    <button className={style2.PurchaseBtn}>Register</button>
+                </Link>
             </div>
         </>
     )
+    }
+
+    if (LoginCtx.isLoggedIn) {
+    return (
+        <>
+            <div className={style.page}>
+                <div style={{ fontSize: "1.75rem", margin: "2.2em 0 0 0", fontWeight: "bold" }}>Home</div>
+                <br/><br/>
+                <Link to="/account">
+                    <button className={style2.PurchaseBtn}>View your account</button>
+                </Link>
+            </div>
+        </>
+    )
+    }
 }
 
 export default DefaultHomePage; 
